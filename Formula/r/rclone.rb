bf96889e974cf25ae41f185ef74c4f0bb90640f6
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-09 13:32:05 +0000

    rclone: add 1.74.3 bottle.

--- Formula/r/rclone.rb
+++ Formula/r/rclone.rb
@@ -6,6 +6,17 @@ class Rclone < Formula
   license "MIT"
   head "https://github.com/rclone/rclone.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/otsge/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90c58d3e1a97270fd457ff5bc2b7367170fdfc7ffda19a736beeaef84124a9b4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc3f8a0bef99f1bbf2daa30b864f72bc8d08bc73a603337e7b950acc162c2874"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "86c930bae38ace124df7ee71dddd88a4159c13491e2916c4ca1daf591f38226a"
+    sha256 cellar: :any_skip_relocation, tahoe:         "8a8f214b759691e126646104edd86fc6fe51f9ecde40e8e5ca4c74e703975780"
+    sha256 cellar: :any_skip_relocation, sequoia:       "e1fd0332bcc333ac423b3ff20bae2af40ad054661a861a06df1702132e985836"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ee7c8abba557cf14b896e7a237edfc07fb379f6da4d7a3a611bf9602589e3124"
+    sha256 cellar: :any,                 x86_64_linux:  "de5aa0f0700b1ddc8b45efba2f5b6c5b1211583f2e4d3ddf41eae44ef9fd0e63"
+  end
+
   depends_on "go" => :build
 
   on_linux do
