77f00d2460491733bcc39cb6807ed5c761d2afd0
John Faust <45057319+slickag@users.noreply.github.com>
2026-06-12 06:46:10 +0000

    zerobrew 0.3.2

--- Formula/z/zerobrew.rb
+++ Formula/z/zerobrew.rb
@@ -1,8 +1,8 @@
 class Zerobrew < Formula
   desc "Fast package manager alternative to Homebrew, written in Rust"
   homepage "https://github.com/lucasgelfond/zerobrew"
-  url "https://github.com/lucasgelfond/zerobrew/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "e35b4f20a04866e67c553e2467f9f57e254b67ada1a2e53c74aa9fbf174f5a3d"
+  url "https://github.com/lucasgelfond/zerobrew/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "696fb9028a4b553fe87eb58af81f44f0676312e07ed89be78fc0886f1f3127a5"
   license all_of: ["Apache-2.0", "MIT"]
   head "https://github.com/lucasgelfond/zerobrew.git", branch: "main"
 
