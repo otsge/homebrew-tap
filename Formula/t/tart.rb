7c5fd7b6301f58a4bf34fc220c479c954e0c0986
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-16 01:24:04 +0000

    tart 2.32.1 (new formula)
    Closes #6.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

---
+++ Formula/t/tart.rb
@@ -0,0 +1,52 @@
+class Tart < Formula
+  desc "Run macOS and Linux VMs on Apple Hardware"
+  homepage "https://github.com/openai/tart"
+  url "https://github.com/openai/tart/archive/refs/tags/2.32.1.tar.gz"
+  sha256 "7583628a566f8e24cd17f6bd14c4973f574de574da3e4b84d303da0a25092da4"
+  license "FSL-1.1-ALv2"
+  head "https://github.com/openai/tart.git", branch: "main"
+
+  depends_on xcode: ["16.3", :build]
+  depends_on :macos
+
+  uses_from_macos "swift" => :build, since: :sequoia # swift 6.3+
+
+  def install
+    arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+
+    args = %W[
+      --arch=#{arch}
+      --configuration=release
+      --disable-sandbox
+    ]
+
+    system "swift", "build", *args
+    mkdir_p ["tart.app/Contents/MacOS", "tart.app/Contents/Resources"]
+    cp ".build/#{arch}-apple-macosx/release/tart", "tart.app/Contents/MacOS/tart"
+    cp Dir["Resources/{embedded.provisionprofile,Info.plist}"], "tart.app/Contents/"
+    cp Dir["Resources/actool/{*.icns,*.car}"], "tart.app/Contents/Resources/"
+    system "strip", "tart.app/Contents/MacOS/tart"
+    libexec.install "tart.app"
+    bin.write_exec_script "#{libexec}/tart.app/Contents/MacOS/tart"
+  end
+
+  def post_install
+    system "xattr", "-cr", libexec.to_s
+    generate_completions_from_executable(bin/"tart", "--generate-completion-script")
+  end
+
+  def caveats
+    <<~EOS
+      Tart has been installed. You might want to reduce the default DHCP lease time
+      from 86,400 to 600 seconds to avoid DHCP shortage when running lots of VMs daily:
+
+        sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.InternetSharing.default.plist bootpd -dict DHCPLeaseTimeSecs -int 600
+
+      See https://tart.run/faq/#changing-the-default-dhcp-lease-time for more details.
+    EOS
+  end
+
+  test do
+    assert_predicate libexec/"tart.app/Contents/MacOS/tart", :executable?
+  end
+end
