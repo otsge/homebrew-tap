f800944016296edef6f548ce61e5ee3aa8328491
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-23 01:37:18 +0000

    qbittorrent-nox: update 5.2.2 bottle.

--- Formula/q/qbittorrent-nox.rb
+++ Formula/q/qbittorrent-nox.rb
@@ -8,12 +8,11 @@ class QbittorrentNox < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/otsge/tap"
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe:   "d74870df8dfcb9a540bef34f6ad7f6cb4a3e50191404fb2620348d7e4ba5d468"
-    sha256 cellar: :any, arm64_sequoia: "cc33f94a7682352f22bb1b3ce97c185ab22d8cb7c40bd45ddaf53c6d4a33c4aa"
-    sha256 cellar: :any, arm64_sonoma:  "4215f4e0c3d6a62b4f9e0693c98ae6c76eb934163a2ea9242abb0123728a5b5a"
-    sha256 cellar: :any, tahoe:         "e1a2dec4d34d9f14a094c8fcabb6ff698dba987a624969fa2e0d501883bbf5e4"
-    sha256 cellar: :any, sequoia:       "f94fcb2612c2b13080791d364f442cf71f1286ceb7196ac558bb6358e2c40f91"
+    sha256 cellar: :any, arm64_tahoe:   "b92fd60531b77cc28d91e3cd3c50c14a63088c17e2b96b908108b6f6650f740f"
+    sha256 cellar: :any, arm64_sequoia: "c5c9b6aa3a3161361df7aec9d43a23bfb2286aa36b1a23eb46d3268941e46a01"
+    sha256 cellar: :any, arm64_sonoma:  "b5b97d032edfeb5e9c3cce60664ad6849e0302f3c24810185340a588a17acf8b"
+    sha256 cellar: :any, tahoe:         "6cf6a915014f538af0d90fd2f7508f1ab618cc985657e9e2f5c05fca7f32381c"
+    sha256 cellar: :any, sequoia:       "8c8f7fee909c70a62b1d8038549721989dbd323313932f0aff055d1027dc129b"
   end
 
   depends_on "boost" => :build
