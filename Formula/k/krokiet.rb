314f03507fa86e99cefd64ff82fa407b15c097ac
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-08 10:30:08 +0000

    krokiet: add 12.0.0 bottle.

--- Formula/k/krokiet.rb
+++ Formula/k/krokiet.rb
@@ -6,6 +6,17 @@ class Krokiet < Formula
   license all_of: ["MIT", "CC-BY-4.0"]
   head "https://github.com/qarmin/czkawka.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/otsge/tap"
+    sha256 cellar: :any, arm64_tahoe:   "972c93e2cd29f15e4098aa5a8fcbe590a99c928249c2d780f306eca00da67427"
+    sha256 cellar: :any, arm64_sequoia: "51ca98152be9bebe301c3497f37188d7a9e55ffb74ec77efa7581fbbdc53430a"
+    sha256 cellar: :any, arm64_sonoma:  "5f7e1279d356a33440167ff5946fe2eddfaaa34218c6c0988f8993382f2150ab"
+    sha256 cellar: :any, tahoe:         "08bb883e8d98194035b9f3d92512579d1d39c669d01f0e4b5108e3b3e9a262c6"
+    sha256 cellar: :any, sequoia:       "965424d621385c6ba16547f57a9777bd66b707ddf1e420c30d0ce30ab9a8b3a4"
+    sha256 cellar: :any, arm64_linux:   "960ec1eb1f7ab9aa9472c678bb627e3e21174a4ef9774d49f8d8ae9dbe6e790b"
+    sha256 cellar: :any, x86_64_linux:  "f5fd3e8d0bb498a5b1301851e5fe658935e1754af6d71313da1af0b7521a5381"
+  end
+
   depends_on "rust" => :build
   depends_on "dav1d"
   depends_on "ffmpeg"
