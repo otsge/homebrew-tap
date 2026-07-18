ca637c2dd26fdf8afb4711b3d0ebb201c77dffaa
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-18 02:01:41 +0000

    Rename tart to tart-dev

--- Formula/t/tart-dev.rb
+++ Formula/t/tart-dev.rb
@@ -1,8 +1,8 @@
 class TartDev < Formula
   desc "Run macOS and Linux VMs on Apple Hardware"
   homepage "https://github.com/openai/tart"
-  url "https://github.com/openai/tart/archive/refs/tags/2.33.0.tar.gz"
-  sha256 "65f339eb17db8f21a9404346f3bcfab396dad22f07de52772096dbe1c991c4ec"
+  url "https://github.com/openai/tart/archive/refs/tags/2.32.1.tar.gz"
+  sha256 "7583628a566f8e24cd17f6bd14c4973f574de574da3e4b84d303da0a25092da4"
   license "FSL-1.1-ALv2"
   head "https://github.com/openai/tart.git", branch: "main"
 
@@ -13,6 +13,8 @@ class TartDev < Formula
     sha256 cellar: :any_skip_relocation, tahoe:         "4524d56f27fdbea7a3be04a1af1682207af9fd7bb77deedff9f4218242956f51"
     sha256 cellar: :any_skip_relocation, sequoia:       "1e31f4c44648c487aa77fc19f6cf142e9f95b514623dbc1126206bae56ca4987"
   end
+
+  keg_only :versioned_formula
 
   depends_on xcode: ["16.3", :build]
   depends_on :macos
